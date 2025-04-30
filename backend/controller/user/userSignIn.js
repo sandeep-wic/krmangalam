f714e733061f9c3344ae7bdb7def8498478afcc2
sandeep-wic <sandeep@wic.org.in>
2025-04-30 15:25:08 +0530

    Update userSignIn.js
    ygjhb

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -33,7 +33,7 @@ async function userSignInController(req,res){
 
         const tokenOption = {
             httpOnly : true,
-            secure : true
+            secure : true,
             sameSite: 'None'        
         }
 
