8dc1d00197284e762af62c3e102c98f8db2a1ffc
sandeep-wic <sandeep@wic.org.in>
2025-04-30 15:20:13 +0530

    Update userSignIn.js

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -34,6 +34,7 @@ async function userSignInController(req,res){
         const tokenOption = {
             httpOnly : true,
             secure : true
+            sameSite: 'None'        
         }
 
         res.cookie("token",token,tokenOption).status(200).json({
